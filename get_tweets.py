0de6c961f75a9810c0c18205e12aa009f8a5d7c7
Chirag Davé <chirage@gmail.com>
2013-04-13 16:24:06 -0700

    wip tweets

--- get_tweets.py
+++ get_tweets.py
@@ -1,9 +1,57 @@
 from twitter import *
-from twitter_employee import TwitterEmployee
-from tweet import Tweet
+from models.twitter_employee import TwitterEmployee
+from models.tweet import Tweet
 import time
 
-twits = TwitterEmployee.find()
-num_tweets = 0
+OAUTH_TOKEN = "16999900-Egp9CoOFvp5tGnea7mCAKjxJ77lRiVlrt4vfiRPYZ"
+OAUTH_SECRET= "0yXs7yomSsZlXdVDKIO7L7fuWBMn1pmMYB1wbw5E9eM"
+CONSUMER_KEY = "uFuIq77xQTgrCXhAj4ugDw"
+CONSUMER_SECRET = "MJKUc3IyN5aztJCrQ8kTI708mPao76dSNGONZ6P6I"
 
-for twit in twits:
+t = Twitter(
+    auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,
+               CONSUMER_KEY, CONSUMER_SECRET)
+    )
+
+employees = TwitterEmployee.find()
+num_employees = employees.count()
+num_tweets = 0
+max_id_dict = {}
+
+print "Starting Process at: ", time.localtime()
+while num_tweets < 100000:
+  # cycle over employees
+  employee = employees[num_tweets % num_employees]
+  sn = employee['screen_name']
+  max_id = max_id_dict.get(sn,None)
+  if max_id:
+    tweets = t.statuses.user_timeline(screen_name=sn,count=200,max_id=max_id)
+  else:
+    tweets = t.statuses.user_timeline(screen_name=sn, count=200)
+
+  # save max_id for next query
+  # subtract 1 because we have 64-bit ints
+  # see: https://dev.twitter.com/docs/working-with-timelines
+  max_id_dict[sn] = tweets[-1]['id'] - 1
+  for tweet in tweets:
+    new_tweet = Tweet(retweet_count=tweet['retweet_count'],
+                      favorites_count=tweet.get('favourites_count', 0),
+                      created_at=tweet['created_at'],
+                      text=tweet['text'],
+                      user_id=employee.id)
+    new_tweet.save()
+    if '_id' in new_tweet:
+      num_tweets += 1
+
+  # user_timeline can be called 180 times every 15 minutes
+  # so wait 5 seconds between calls so we don't hit rate limit
+  time.sleep(5)
+  print "Progress Report--\n", "\tTweets: ", num_tweets, "\n\tCurr User: ", sn
+
+print "Completed at: ", time.localtime()
+
+
+
+
+
+
--- models/tweet.py
+++ models/tweet.py
@@ -1,2 +1,13 @@
-def hello():
-  print 'hello'
+from mongo import Document, Index
+
+class Tweet(Document):
+  """Twitter Tweet"""
+  __database__ = 'addvocate_exam'
+  __collection__ = 'tweets'
+  __indexes__ = [
+    Index('retweet_count'),
+    Index('favorites_count'),
+    Index('user_id'),
+    Index('created_at'),
+    Index('twitter_id', unique=True)
+  ]
