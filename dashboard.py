f8a76380e892f5e01a91819a4e3360f310cfc5ed
Chirag Davé <chirage@gmail.com>
2013-04-15 20:39:22 -0700

    need to draw pictures now

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask import render_template
 from pymongo import MongoClient
+from bson.son import SON
+from bson import json_util
+import json
 
 app = Flask(__name__)
 
@@ -8,24 +12,29 @@ db = client.addvocate_exam
 Tweets = db.tweets
 Employees = db.twitter_employees
 
-@app.route("/top-users")
+@app.route("/")
+def dashboard():
+  return render_template("dashboard.html")
+
+@app.route("/top-users.json")
 def top_users():
-  top_users =  Tweets.aggregate({
-      "$group": { 
+  top_users =  Tweets.aggregate([
+      {"$group": {
         "_id": "$user_id", 
-        "total_rts": { "$sum": "$retweet_count"}, 
-        "total_fav": {"$sum": "$favorites_count"}}})
-  return str(top_users)
+        "total_rts": {"$sum": "$retweet_count"}, 
+        "total_fav": {"$sum": "$favorites_count"}}},
+      {"$sort": SON([("total_rts", -1), ("total_fav", -1)])}
+    ])
+  return json.dumps(top_users, default=json_util.default)
 
 
-@app.route("/top-content")
+@app.route("/top-content.json")
 def top_content():
   top_content = Tweets.find() .sort("retweet_count", -1) .sort("favorites_count", -1).limit(15)
-  print 'debug: ', top_content
-  results = ''
+  results = []
   for tweet in top_content:
-    results += str(tweet)
-  return results
+    results.append(tweet)
+  return json.dumps(results, default=json_util.default)
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
